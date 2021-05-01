6d47cd8d08ac4c34c93fdf69e99ae90dafe3eba1
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-05-01 22:40:00 +0600

    Head added in problem pages

--- developer-hunt/pages/problems/[id].js
+++ developer-hunt/pages/problems/[id].js
@@ -1,6 +1,11 @@
+import Head from 'next/head'
 const ProblemDetails = () => {
     return (
         <div>
+            <Head>
+                <title>Problem Details</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <h1>This is the details page</h1>
         </div>
     );
--- developer-hunt/pages/problems/index.js
+++ developer-hunt/pages/problems/index.js
@@ -1,8 +1,12 @@
 import ProblemThumbnail from "../../components/problems/ProblemThumbnail"
-
+import Head from 'next/head'
 const problems = () => {
     return (
         <div>
+            <Head>
+                <title>Problems | Developer Hunt</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             {/* problem list with the ProblemThumbnail */}
             <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
