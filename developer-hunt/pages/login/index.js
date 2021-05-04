23d9ffff3752c843e2f88969d40ff4df5c8c6351
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-05-05 03:01:39 +0600

    Update index.js

--- developer-hunt/pages/login/index.js
+++ developer-hunt/pages/login/index.js
@@ -1,16 +1,30 @@
+import { Player } from '@lottiefiles/react-lottie-player';
 import Head from "next/head";
-const Login = () => {
+import React, { useState } from 'react'
+
+function Login() {
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
   return (
     <div>
       <Head>
         <title>Login | Developer Hunt</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="p-2 bg-blue-300 w-20 h-auto text-black rounded-xl text-center">
-        Sign Up
-      </div>
-    </div>
-  );
-};
 
-export default Login;
+      <div className="grid sm:grid-cols-1  xl:grid-cols-2 items-center my-10">
+        <Player
+          autoplay
+          loop
+          src="https://assets2.lottiefiles.com/packages/lf20_i98z7y0a.json"
+        />
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
+
