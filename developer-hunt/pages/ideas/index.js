c5d82ab837621544f8d44e6ddf5332fd6742ccde
SumayaSiddique <73577002+SumayaSiddique@users.noreply.github.com>
2021-05-04 22:36:41 +0600

    Idea Page Updated

--- developer-hunt/pages/ideas/index.js
+++ developer-hunt/pages/ideas/index.js
@@ -1,15 +1,29 @@
-import Head from 'next/head'
+import { Player } from "@lottiefiles/react-lottie-player";
+import Head from "next/head";
 const Ideas = () => {
-
-    return (
+  return (
+    <div>
+      <Head>
+        <title>Ideas | Developer Hunt</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="grid sm:grid-cols-1  xl:grid-cols-2 items-center my-10">
         <div>
-            <Head>
-                <title>Ideas | Developer Hunt</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
+          <p className="text-5xl font-mono my-5 ">
+            Pulling out your hair for a bug?
+          </p>
+          <p className="text-xl font-mono text-gray-500">
+            Come, seek help to your new online colleagues
+          </p>
         </div>
-    )
-
-}
+        <Player
+          autoplay
+          loop
+          src="https://assets4.lottiefiles.com/packages/lf20_0cvczw8l.json"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Ideas;
