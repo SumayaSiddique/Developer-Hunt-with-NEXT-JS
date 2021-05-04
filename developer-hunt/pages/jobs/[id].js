8acb32cd98edf332012792918a6ccd8ad3aafdbc
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-05-04 20:56:35 +0600

    job page with details

---
+++ developer-hunt/pages/jobs/[id].js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+const ProblemDetails = () => {
+    return (
+        <div>
+            <Head>
+                <title>Job Details | Developer Hunt</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <h1>This is the Job details page</h1>
+        </div>
+    );
+}
+
+export default ProblemDetails;
