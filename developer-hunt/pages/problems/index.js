023f4b8f13fe2e891184396d58f0696d1fb50944
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-05-04 20:56:10 +0600

    problem page with details added

--- developer-hunt/pages/problems/index.js
+++ developer-hunt/pages/problems/index.js
@@ -1,3 +1,4 @@
+import { Player } from '@lottiefiles/react-lottie-player';
 import ProblemThumbnail from "../../components/problems/ProblemThumbnail"
 import Head from 'next/head'
 const problems = () => {
@@ -7,6 +8,19 @@ const problems = () => {
                 <title>Problems | Developer Hunt</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
+            <div className="grid sm:grid-cols-1  xl:grid-cols-2 items-center my-10">
+                <div>
+                    <p className="text-5xl font-mono my-5 ">Pulling out your hair for a bug?</p>
+                    <p className="text-xl font-mono text-gray-500">Come, seek help to your new online colleagues</p>
+
+                </div>
+                <Player
+                    autoplay
+                    loop
+                    src="https://assets8.lottiefiles.com/private_files/lf30_foIGdO.json"
+                />
+
+            </div>
             {/* problem list with the ProblemThumbnail */}
             <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
